ccfed8d0c35f8a96d66f6f4e77b052caa9d53d75
TuanAnhhg <50505813+TuanAnhhg@users.noreply.github.com>
2019-09-29 15:29:34 +0700

    Add files via upload

---
+++ AddToCart.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Diagnostics;
+using BooksShopOnline.Logic;
+namespace BooksShopOnline
+{
+    public partial class AddToCard : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string rawId = Request.QueryString["BookID"];
+            int bookId;
+            if (!String.IsNullOrEmpty(rawId) && int.TryParse(rawId, out bookId))
+            {
+                using (ShoppingCartActions usersShoppingCart = new
+                ShoppingCartActions())
+                {
+                    usersShoppingCart.AddToCart(Convert.ToInt16(rawId));
+                }
+            }
+
+            Response.Redirect("ShoppingCart.aspx");
+        }
+    }
+}
