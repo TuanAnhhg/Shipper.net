ca54a4fe9cb5f3732d13467c34694bec0b9fce93
TuanAnhhg <50505813+TuanAnhhg@users.noreply.github.com>
2019-10-15 10:27:19 +0700

    Add files via upload

---
+++ Context.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace BooksShopOnline.Models
+{
+    public class Context : DbContext
+    {
+        public Context() : base("BooksShopOnline")
+        { }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<hang> Books { get; set; }
+    }
+}
---
+++ hang.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BooksShopOnline.Models;
+using System.Web.ModelBinding;
+
+namespace BooksShopOnline
+{
+    public partial class hang: System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public IQueryable<Models.hang> GetBooks([QueryString("id")] int? categoryId)
+        {
+            var _db = new BooksShopOnline.Models.Context();
+            IQueryable<Models.hang> query = _db.Books;
+            if (categoryId.HasValue && categoryId > 0)
+            {
+                query = query.Where(p => p.CategoryID == categoryId);
+            }
+            return query;
+        }
+    }
+}
